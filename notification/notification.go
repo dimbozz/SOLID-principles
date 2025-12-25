10d2488d28451d71b7d034718ad30f279e74e8da
Dmitriy <dmitriy.tsaberyabyy@kuper.ru>
2025-12-25 20:37:45 +0300

    Initial commit

---
+++ notification/notification.go
@@ -0,0 +1,12 @@
+package notification
+
+// NotificationService — только отправка уведомлений
+type NotificationService struct{}
+
+func NewNotificationService() *NotificationService {
+	return &NotificationService{}
+}
+
+func (n *NotificationService) SendEmailNotification (customer string) {
+	println("Уведомление отправлено клиенту", customer)
+}
